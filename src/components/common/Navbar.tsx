0386a43b03ad969648ef37ae30faf71a77a3ef50
federicojaime <federiconj@gmail.com>
2025-03-09 23:12:31 -0300

    add Tailwind CSS configuration and update Navbar styles

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar = () => {
   ];
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-40 w-full border-b backdrop-blur ">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Link to="/" className="flex items-center space-x-2">
